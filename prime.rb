3b1c88728914f55bdc8d5c07d2a46edc84bd7ee1
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-10 01:20:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(num)
-  if (num<=0 || num=1)
+  if (num<=0 )
     flag=false
   else 
    flag = true
-    x=2
-    while (x<=num)
+   x=2
+    while (x<=num/2 )
       if  (num % x ==0)
        flag =false
       break
