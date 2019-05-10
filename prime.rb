d155004465a4a2fc811914354de1468f826a1158
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-10 01:16:14 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,17 @@
+def prime?(num)
+  if (num<=0)
+    flag=false
+  else 
+   flag = true
+   x=2
+    while (x<=num/2 )
+      if  (num % x ==0)
+       flag =false
+      break
+    end
+    x +=1
+  end
+    return flag
+  end
+
+end
